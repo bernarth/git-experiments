1b97e6c01c88a3c7dc1b97f3fdcff985cb982af8
Bernarth <alberthbernarth75@gmail.com>
2022-06-13 21:43:19 -0400

    add a sort string function from feature

---
+++ app/helper/strings.js
@@ -0,0 +1,23 @@
+// sort string by alphabetical order
+const sortString = (str) => {
+  return str.split('').sort().join('');
+}
+
+// trim string
+const trimString = (str) => {
+  return str.trim();
+}
+
+// remove duplicate string
+const removeDuplicateString = (str) => {
+  return str.split('').filter((item, index) => {
+    return str.indexOf(item) === index;
+  }).join('');
+}
+
+// find vowel in string
+const findVowel = (str) => {
+  return str.split('').filter((item) => {
+    return item.match(/[aeiou]/i);
+  }).join('');
+}
