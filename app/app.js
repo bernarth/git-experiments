55df68cb72fa521e6004844b80a526a22d041a6a
Bernarth <alberthbernarth75@gmail.com>
2022-06-13 21:41:33 -0400

    commit 1 from main

--- app/app.js
+++ app/app.js
@@ -5,3 +5,17 @@ const fibonnaci = (n) => {
   }
   return fibonnaci(n - 1) + fibonnaci(n - 2);
 }
+
+// sort array of numbers
+const sort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        }
+    }
+    return arr;
+}
