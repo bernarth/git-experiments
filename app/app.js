aff6295437d53c6b0df9b7665346bac303b4379f
Bernarth <alberthbernarth75@gmail.com>
2022-09-26 10:36:59 -0400

    Remove function to remove duplicates from strings and Style

--- app/app.js
+++ app/app.js
@@ -3,19 +3,21 @@ const fibonnaci = (n) => {
   if (n <= 1) {
     return n;
   }
+
   return fibonnaci(n - 1) + fibonnaci(n - 2);
 }
 
 // sort array of numbers
 const sort = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = i + 1; j < arr.length; j++) {
-        if (arr[i] > arr[j]) {
-            let temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+          let temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
       }
     }
-    return arr;
+  }
+
+  return arr;
 }
